a855d0ba87c461a87fdd757ba29742d9918101e4
kritikavaishnav0506 <kritikavaishnav0506@gmail.com>
2026-09-04 19:15:40 +0530

    Add files via upload
    pr

---
+++ pras1-2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int TR,TO;
+    float R;
+    cout<<"Cricket Score Card"<<endl;
+    cout<<"-----------------------------"<<endl;
+    cout<<left<<setw(15)<<"Total Run"<<" :";
+    cin>>TR;
+    cout<<left<<setw(15)<<"Total Over"<<" :";
+    cin>>TO;
+    cout<<left<<setw(15)<<"Run Rate"<<" :";
+    cin>>R;
+
+}
